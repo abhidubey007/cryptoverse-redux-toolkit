86563ba5f7752d189f6e208c294d2d208c20ae1b
abhidubey007 <abhidb95@gmail.com>
2022-09-29 18:19:08 +0530

    Pages Setup Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React from 'react';
+import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
 
-const App = () => {
+function App() {
     return (
-        <>
-            <h1>Cryptoverse</h1>
-        </>
-    )
+        <div className="app">
+            <div className='navbar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route path='/' element={<Homepage />} />
+                            <Route path='/exchanges' element={<Exchanges />} />
+                            <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                            <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+                            <Route path='/news' element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+                <div className='footer'>
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptoverse <br />
+                        All rights reserved @Abhisek Dubey 2022
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
