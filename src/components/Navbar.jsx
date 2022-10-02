b4c2b2d207f8feefec884796fef77d1ba6a910b0
abhidubey007 <abhidb95@gmail.com>
2022-10-02 12:09:36 +0530

    Home page Done, cryptoDetails left

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Button, Menu, Typography, Avatar } from 'antd'
+import { Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, LinkedinOutlined } from '@ant-design/icons'
 
 import icon from '../images/cryptocurrency.png'
 
@@ -27,6 +27,9 @@ const Navbar = () => {
                 </Menu.Item>
                 <Menu.Item icon={<BulbOutlined />}>
                     <Link to='/news'>News</Link>
+                </Menu.Item>
+                <Menu.Item icon={<LinkedinOutlined />}>
+                    <a href='https://www.linkedin.com/in/abhisekdubey'>Abhisek Dubey (Developer)</a>
                 </Menu.Item>
             </Menu>
         </div>
